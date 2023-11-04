38caf97ef870523671b7a2bf1193f0459dac8eef
Rt0x <144723959+Rt0x@users.noreply.github.com>
2023-11-04 22:21:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function calc() {
-    var type_cars = document.getElementById("type_cars");
+    var type_flowers = document.getElementById("type_flowers");
     var count = document.getElementById("count").value;
     if(count==""){alert("Вы не указали количество");}
     else if(count<0){alert("Количество не может быть отрицательным");}
     else{
       var price = 0;
-      price += parseInt(type_cars.options[type_cars.selectedIndex].value);
+      price += parseInt(type_flowers.options[type_flowers.selectedIndex].value);
       price = parseInt(count) * price;
       document.getElementById('result').innerHTML = "Стоимость равна: "+ price +" р.";
     }
