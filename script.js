658db032221fca9978be4cef33921a2b9cb408b9
Rt0x <144723959+Rt0x@users.noreply.github.com>
2023-11-04 22:05:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,49 @@
-function calc() {
-    var type_pie = document.getElementById("type_pie");
-    var count = document.getElementById("count").value;
-    if (isNaN(count)) {alert("Некорректное значение");return false;}
-    let chek = document.querySelector('#glaze');
-    const form = document.getElementById("radios");
-    if(count==""){alert("Вы не указали количество");}
-    else if(count<0){alert("Количество не может быть отрицательным");}
-    else{
-        var price = 0;
-        if (form.elements["toward"].value=="125"){
-            price += parseInt(form.elements["toward"].value);
-            price = parseInt(count) * price;
-            document.getElementById('result').innerHTML = "Стоимость равна: "+ price +" р.";}
-        else if(form.elements["toward"].value=="45"){
-            price += parseInt(form.elements["toward"].value);
-            price += parseInt(type_pie.options[type_pie.selectedIndex].value);
-            price = parseInt(count) * price;
-            document.getElementById('result').innerHTML = "Стоимость равна: "+ price +" р.";
-        }
-        else if(form.elements["toward"].value=="100"){
-            price += parseInt(form.elements["toward"].value);
-            price = parseInt(count) * price;
-            if(chek.checked){price+=25;}
-            document.getElementById('result').innerHTML = "Стоимость равна: "+ price +" р.";}
+const radios = document.querySelectorAll('input[type="radio"]');
+const but = document.querySelector('button');
+
+function calculateCost() {
+  let size = 0;
+  let crust = 0;
+  let toppings = 0;
+
+  // Get selected size
+  for (let i = 0; i < radios.length; i++) {
+    if (radios[i].checked) {
+      size = parseFloat(radios[i].value);
+      break;
     }
+  }
+
+  // Get selected crust
+  const crustSelect = document.getElementById('crust');
+  crust = parseFloat(crustSelect.value);
+
+  // Get selected toppings
+  const toppingsSelect = document.getElementById('toppings');
+  toppings = parseFloat(toppingsSelect.value);
+
+  // Calculate total cost
+  const totalCost = size + crust + toppings;
+
+  // Display total cost
+  const costDisplay = document.getElementById('cost');
+  costDisplay.textContent = totalCost.toFixed(2);
 }
 
-function view(event){
-    var type_pie = document.getElementById("type_pie");
-    let chek = document.querySelector('#glaze');
-    const form = document.getElementById("radios");
-    if (form.elements["toward"].value=="125"){
-        document.getElementById("selection").style.display="none";
-        document.getElementById("chec").style.display="none";
-    }
-    else if(form.elements["toward"].value=="45"){
-        document.getElementById("selection").style.display="block";
-        document.getElementById("chec").style.display="none";
-    }
-    else if(form.elements["toward"].value=="100"){
-        document.getElementById("selection").style.display="none";
-        document.getElementById("chec").style.display="block";
-    }
+function toggleFields() {
+  const extraFields = document.getElementById('extra-fields');
+  
+  if (but.checked) {
+    extraFields.style.display = 'block';
+  } else {
+    extraFields.style.display = 'none';
+  }
 }
 
-document.addEventListener("DOMContentLoaded", function(){
-    radios.addEventListener('change', view);
-    but.addEventListener('click', calc);
+// Add event listener to calculate button
+but.addEventListener('click', calculateCost);
+
+// Add event listener to toggle button
+radios.forEach((radio) => {
+  radio.addEventListener('change', toggleFields);
 });
